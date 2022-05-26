91f1942d765eec50b7a38b8f6ab61bb8c4fc8034
oguzhankartal <kartaloguzhan12@gmail.com>
2022-05-26 14:14:55 +0200

    printCard function added

--- CardGame/CardGame.cpp
+++ CardGame/CardGame.cpp
@@ -31,6 +31,46 @@ enum class CardRank
     max_ranks
 };
 
+struct Card {
+    CardRank rank{};
+    CardSuit suit{};
+};
+
+void printCard(const Card& card)
+{
+    switch (card.rank)
+    {
+        case CardRank::rank_2:      std::cout << '2';   break;
+        case CardRank::rank_3:      std::cout << '3';   break;
+        case CardRank::rank_4:      std::cout << '4';   break;
+        case CardRank::rank_5:      std::cout << '5';   break;
+        case CardRank::rank_6:      std::cout << '6';   break;
+        case CardRank::rank_7:      std::cout << '7';   break;
+        case CardRank::rank_8:      std::cout << '8';   break;
+        case CardRank::rank_9:      std::cout << '9';   break;
+        case CardRank::rank_10:     std::cout << 'T';   break;
+        case CardRank::jack:        std::cout << 'J';   break;
+        case CardRank::queen:       std::cout << 'Q';   break;
+        case CardRank::king:        std::cout << 'K';   break;
+        case CardRank::ace:         std::cout << 'A';   break;
+        default:
+            std::cout << '?';
+            break;
+    }
+
+    switch (card.suit)
+    {
+        case CardSuit::club:       std::cout << 'C';   break;
+        case CardSuit::diamond:    std::cout << 'D';   break;
+        case CardSuit::heart:      std::cout << 'H';   break;
+        case CardSuit::spade:      std::cout << 'S';   break;
+        default:
+            std::cout << '?';
+            break;
+    }
+}
+
+
 int main()
 {
     std::cout << "Hello World!\n";
