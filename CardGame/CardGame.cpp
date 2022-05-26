640e43ed005f65750b8e52665d72d372f9a79f4d
oguzhankartal <kartaloguzhan12@gmail.com>
2022-05-26 14:08:13 +0200

    Enum classes created for suit and ranks

--- CardGame/CardGame.cpp
+++ CardGame/CardGame.cpp
@@ -2,6 +2,35 @@
 //
 #include <iostream>
 
+enum class CardSuit
+{
+    club,
+    diamond,
+    heart,
+    spade,
+
+    max_suits
+};
+
+enum class CardRank
+{
+    rank_2,
+    rank_3,
+    rank_4,
+    rank_5,
+    rank_6,
+    rank_7,
+    rank_8,
+    rank_9,
+    rank_10,
+    jack,
+    queen,
+    king,
+    ace,
+
+    max_ranks
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
